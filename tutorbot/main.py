2cd3c4baba79c18711a8548f7df04b7dbd7a9e00
Daniel Buckmaster <dan.buckmaster@gmail.com>
2016-01-06 09:14:45 +1100

    Worked on tutorial

--- tutorbot/main.py
+++ tutorbot/main.py
@@ -40,45 +40,85 @@ def tutorbot(slack, my_id):
                         if tutor is None:
                             tutor = message['user']
                         else:
-                            say(slack, message['channel'], 'Nice try')
+                            send_message(slack, message['channel'], 'Nice try')
                     else:
                         handle_private_message(slack, message, running_tutorials)
                 elif mentioned.match(message['text']) is not None:
                     handle_public_message(slack, message)
 
 def handle_public_message(slack, message):
-    say(slack, message['channel'], "<@"+message['user']+">, come talk to me in https://ncss2016bots.slack.com/messages/@tutorbot/")
+    send_message(slack, message['channel'], "<@"+message['user']+">, come talk to me in https://ncss2016bots.slack.com/messages/@tutorbot/")
 
 def handle_private_message(slack, message, tutorials):
     user = message['user']
     if user in tutorials:
         try:
             tutorials[user].send(message)
-        except StopIteration:
+        except (StopIteration, UserQuit):
             del tutorials[user]
-            say(slack, message['channel'], 'Bye!')
+            send_message(slack, message['channel'], 'Bye!')
+        except:
+            del tutorials[user]
+            send_message(slack, message['channel'], 'Something went wrong!')
     else:
         tutorial = run_tutorial(slack, user, message['channel'])
         tutorial.send(None)
         tutorials[user] = tutorial
 
+def example(text):
+    return text[1:]
+
+step1script = example('''
+import slackclient
+
+token = 'your Slack API token here!'
+client = slackclient.SlackClient(token)
+if client.rtm_connect():
+    print('connected!')
+else:
+    print('failed for some reason :(')
+''')
+
+step2script = example('''
+    while True:
+        messages = client.rtm_read()
+        for message in messages:
+            print(message)
+'''
+
+step3script = example('''
+            if message['type'] != 'message':
+                continue
+            if 'robots suck' in message['text']:
+                client.rtm_send_message(message['channel'], 'I heard that!')
+'''
+
+class UserQuit(Exception):
+    pass
+
 @asyncio.coroutine
 def run_tutorial(slack, pupil, channel):
-    say(slack, channel, "Hey there! If you'd like me to start the tutorial, send me a message with 'begin' in it!")
+    def say(text, **kwargs):
+        if len(kwargs) == 0:
+            slack.rtm_send_message(channel, text)
+        else:
+            send_message(slack, channel, text, **kwargs)
+
+    say("Hey there! If you'd like me to start the tutorial, send me a message with 'begin' in it!")
     msg = yield
     if 'begin' not in msg['text'].lower():
-        say(slack, channel, "Never mind then. Come back any time! :upside_down_face:")
+        say("Never mind then. Come back any time! :upside_down_face:")
         raise StopIteration()
 
-    say(slack, channel, (
+    say(
         "Yay! :smile:"
         " At any time during the tutorial, you can send me 'quit' to stop."
         " Got it?"
-    ))
+    )
 
     yield from next_message()
 
-    say(slack, channel, (
+    say((
         "The first thing you'll need to do to make a Slack bot is get an API token."
         " This is like a password that lets you send and receive messages."
         " Every bot needs its own token."
@@ -90,54 +130,95 @@ def run_tutorial(slack, pupil, channel):
     ), attach_image = 'http://i.imgur.com/JAO12nm.png')
 
     yield from next_message()
-    say(slack, channel, (
+    say(
         "Copy the API token somewhere safe!"
         " You could even paste it here and I'll remember it for you."
-        " Once you've set up your bot (choose a nice emoticon, won't you? :scream:), click 'Save integration'. Let me know once that's done."
-    ))
+        " Once you've set up your bot (choose a nice emoticon, won't you? :scream:), click `Save integration`. Let me know once that's done."
+    )
 
     yield from next_message()
-    say(slack, channel, (
+    say(
         "Okay, great. Now it's time to write some Python!"
         " Create a new Python file in IDLE, and we'll start by importing the `slackclient` library and connecting to the realtime API!"
         " It looks something like this:"
-    ))
+    )
 
     send_file(slack, channel,
         name = 'step 1.py',
         filetype = 'python',
-        content = '\n'.join([
-            'import slackclient',
-            '',
-            'token = \'your Slack API token here!\'',
-            'client = slackclient.SlackClient(token)',
-            'if client.rtm_connect():',
-            '    print(\'connected!\')',
-            'else:',
-            '    print(\'failed for some reason :(\')',
-        ]),
+        content = step1script,
     )
 
-    say(slack, channel, "Let me know when you're ready to continue.")
+    say(
+        "Try running this - it should print out `connected!`."
+        " Let me know when you're ready to continue."
+    )
+
+    yield from next_message()
+    say((
+        "Now that you know how to connect to Slack, let's start listening to some messages."
+        " Bots can only see messages from channels they've been invited into (like vampires), so before you do this, invite your bot into #robots."
+        " Here's Daniel inviting me by @mentioning me in the #robots channel:"
+    ), attach_image = 'http://i.imgur.com/2wybb1W.png')
+
+    say(
+        "Once you've done that, we can add a little more code to start listening for messages."
+        " Put this inside the `if` branch after `print('connected!')`:"
+    )
+    send_file(slack, channel,
+        name = 'step 2.py',
+        filetype = 'python',
+        content = step2script,
+    )
+    say(
+        "If you run that code, then send a message in the #robots channel, and you should see it printed out by your program!"
+        " The message will appear as a JSON object."
+        " JSON objects are just like Python dicts."
+        " You'll see several different fields in each message."
+        " Try running the code and let me know how it goes!"
+    )
+
+    yield from next_message()
+    say(
+        "Notice there are messages with several different `type`s, and they each have different contents."
+        " You can see a list of all the possible message types at https://api.slack.com/rtm."
+        " Now that we can see messages coming in, let's respond to them!"
+        " Tell me when I should keep going."
+    )
+
+    yield from next_message()
+    say(
+        "Instances of the `SlackClient` class have a `rtm_send_message` method which we can use to quickly send messages to a channel"
+        " You can use it like this: `client.rtm_send_message('channel ID', 'message here!')`."
+        " The tricky part is finding the right channel ID for a particular message."
+        " For now, we'll write a bot that only responds to messages in the same channel they appeared in, so we can get the channel out of the message object, like this:"
+    )
+    send_file(slack, channel,
+        name = 'step 3.py',
+        filetype = 'python',
+        content = step3script,
+    )
+    say(
+        "Notice that we're ignoring events that aren't messages."
+        " Those messages might not have a `text` property, so we wouldn't know what to do with them!"
+        " Replace `print(message)` in your file with that code, give it a try, and let me know how you get on!"
+    )
 
     yield from next_message()
 
 @asyncio.coroutine
 def next_message():
-    message = None
-    while message is None:
-        message = yield
-        text = message['text'] = message['text'].lower()
-        if 'exit' in text or 'quit' in text:
-            raise StopIteration()
-        if 'help' in text or 'how do i' in text or 'can\'t' in text:
-            print('Help requested?', text)
-        elif text.startswith('feedback:'):
-            print(text)
-            message = None
+    message = yield
+    text = message['text'] = message['text'].lower()
+    if 'exit' in text or 'quit' in text:
+        raise UserQuit()
+    if 'help' in text or 'how do i' in text or 'can\'t' in text:
+        print('==============================')
+        print('Help requested?', text)
+        print('==============================')
     return message
 
-def say(slack, channel, text, attach_image = None):
+def send_message(slack, channel, text, attach_image = None):
     attachments = []
     if attach_image is not None:
         attachments.append({
